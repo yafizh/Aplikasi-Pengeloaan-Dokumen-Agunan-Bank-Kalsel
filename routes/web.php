357234d6cf6fb47b9e8c5284659a3932ed9e68e5
yafizh <a.chairil.yafizh@gmail.com>
2025-02-18 17:34:07 +0700

    auth middleware

--- routes/web.php
+++ routes/web.php
@@ -9,33 +9,37 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
-Route::resource('/lemari', LemariController::class);
-Route::resource('/lemari-detail', LemariDetailController::class);
-Route::resource('/dokumen-agunan', DokumenAgunanController::class);
-Route::resource('/dokumen-agunan-peminjaman', DokumenAgunanPeminjamanController::class);
-Route::controller(ReportController::class)
-    ->prefix('laporan')
-    ->group(function () {
-        Route::get('/daftar-agunan', 'daftarAgunan');
-        Route::get('/status-verifikasi', 'statusVerifikasi');
-        Route::get('/masa-berlaku', 'masaBerlaku');
-        Route::get('/peminjaman-pengembalian', 'peminjamanPengembalian');
-        Route::get('/letak-dokumen-agunan', 'letakDokumenAgunan');
-        Route::get('/ketersediaan-ruang-penyimpanan', 'ketersediaanRuangPenyimpanan');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
     });
-Route::controller(CetakController::class)
-    ->prefix('cetak')
-    ->group(function () {
-        Route::get('/daftar-agunan', 'daftarAgunan');
-        Route::get('/status-verifikasi', 'statusVerifikasi');
-        Route::get('/masa-berlaku', 'masaBerlaku');
-        Route::get('/peminjaman-pengembalian', 'peminjamanPengembalian');
-        Route::get('/letak-dokumen-agunan', 'letakDokumenAgunan');
-        Route::get('/ketersediaan-ruang-penyimpanan', 'ketersediaanRuangPenyimpanan');
-    });
+
+
+    Route::resource('/lemari', LemariController::class);
+    Route::resource('/lemari-detail', LemariDetailController::class);
+    Route::resource('/dokumen-agunan', DokumenAgunanController::class);
+    Route::resource('/dokumen-agunan-peminjaman', DokumenAgunanPeminjamanController::class);
+    Route::controller(ReportController::class)
+        ->prefix('laporan')
+        ->group(function () {
+            Route::get('/daftar-agunan', 'daftarAgunan');
+            Route::get('/status-verifikasi', 'statusVerifikasi');
+            Route::get('/masa-berlaku', 'masaBerlaku');
+            Route::get('/peminjaman-pengembalian', 'peminjamanPengembalian');
+            Route::get('/letak-dokumen-agunan', 'letakDokumenAgunan');
+            Route::get('/ketersediaan-ruang-penyimpanan', 'ketersediaanRuangPenyimpanan');
+        });
+    Route::controller(CetakController::class)
+        ->prefix('cetak')
+        ->group(function () {
+            Route::get('/daftar-agunan', 'daftarAgunan');
+            Route::get('/status-verifikasi', 'statusVerifikasi');
+            Route::get('/masa-berlaku', 'masaBerlaku');
+            Route::get('/peminjaman-pengembalian', 'peminjamanPengembalian');
+            Route::get('/letak-dokumen-agunan', 'letakDokumenAgunan');
+            Route::get('/ketersediaan-ruang-penyimpanan', 'ketersediaanRuangPenyimpanan');
+        });
+});
