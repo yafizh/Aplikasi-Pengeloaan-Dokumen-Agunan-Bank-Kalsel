7aad78975609d17570af1247cf7af721c7d21482
yafizh <a.chairil.yafizh@gmail.com>
2025-02-20 08:19:35 +0700

    update

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -28,11 +28,11 @@
                         @csrf
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username">
+                            <input type="text" class="form-control" id="username" name="username" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password">
+                            <input type="password" name="password" class="form-control" id="password" required>
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Login</button>
                     </form>
