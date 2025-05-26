c227125bc79f56042f063d877108402ed5e84c7f
yafizh <a.chairil.yafizh@gmail.com>
2025-05-26 18:26:14 +0700

    login page

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,46 +1,71 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
+    <title>Login</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- CSRF Token -->
+    <meta name="_token" content="{{ csrf_token() }}">
+
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+
+    <!-- plugin css -->
+    <link rel="stylesheet" href="/assets/plugins/@mdi/font/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="/assets/plugins/perfect-scrollbar/perfect-scrollbar.css">
+    <!-- end plugin css -->
+
+    <!-- common css -->
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- end common css -->
+
 </head>
 
 <body>
-    <div class="row g-0 mt-5">
-        <div class="col-3"></div>
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <div class="mb-3 w-100 text-center">
-                        <img src="{{ url('assets/images/logo.png') }}" alt="">
+    <div class="container-scroller" id="app">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center justify-content-center auth theme-one"
+                style="background-image: url('https://infoekonomi.id/wp-content/uploads/2024/01/bank-kalsel.webp'); background-size: cover; background-repeat: no-repeat;">
+                <div class="row w-100">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auto-form-wrapper">
+                            <h5 class="text-center mb-3" style="font-weight: bold;">APLIKASI PENGELOLAAN <br> DOKUMEN
+                                AGUNAN</h5>
+                            @if (session('error'))
+                                <div class="alert alert-danger" role="alert">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+                            <form class="mt-3" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label class="label">Username</label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control rounded border" name="username"
+                                            required autofocus>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="label">Password</label>
+                                    <div class="input-group">
+                                        <input type="password" class="form-control rounded border" name="password"
+                                            required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary submit-btn btn-block">Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <h5 class="text-center mb-3">APLIKASI PENGELOLAAN DOKUMEN AGUNAN</h5>
-                    @if (session('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-                    <form class="mt-3" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100">Login</button>
-                    </form>
                 </div>
             </div>
         </div>
-        <div class="col-3"></div>
     </div>
+
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
